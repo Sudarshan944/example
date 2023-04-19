743e7c5266d59a0fb8b308e12af478643ed2611e
Sudarshan <nanireddy99446@gmail.com>
2023-04-19 11:53:10 +0530

    update

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,11 @@
-num=int(input())
-rev=0
-temp=num
-while num>0:
-    rem=num%10
-    rev=rev*10+rem
-    num//=10
-if(rev==temp):
+num = int(input())
+rev = 0
+temp = num
+while num > 0:
+    rem = num % 10
+    rev = rev*10+rem
+    num //= 10
+if (rev == temp):
     print("Palindrome")
 else:
     print("Not a Palindrome")
