ad564c25379c8b8fe0f469f89ad0225407111d92
Sudarshan <nanireddy99446@gmail.com>
2023-04-19 13:04:16 +0530

    a

--- swap3.py
+++ swap3.py
@@ -1,16 +1,12 @@
 n1=int(input())
 n2=int(input())
 n3=int(input())
-if n1>n2 and n1>n3:
+if n1<n2 and n1<n3:
     if n2<n3:
-        temp=n1
-        n1,n2=n2,n3
-        n3=temp
+        print(n1,n2,n3)
     else:
-        temp = n1
-        n1=n3
-        n3 = temp
-elif n1<n3:   ###STILL PENDIND
+        print(n2,n3,n1)
+elif n1<n3:
     if n2<n3:
         print(n2,n1,n3)
     else:
